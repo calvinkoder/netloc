7b02b2081708b6a3a731cd27893365512db71bc9
Calvin Koder <calvinkoder@protonmail.com>
2016-10-26 13:27:34 -0400

    fixed trilat, renamed variables, removed Line

--- trilat.c
+++ trilat.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <float.h>
 
-#define M_TAU M_PI/2
+const float M_TAU = M_PI*2;
 
+int nearly_equal(float a, float b, float epsilon){
+	float diff = fabs(a - b);
+	if (a == b){
+		return 1;
+	}
+	else if ( a == 0 || diff < FLT_MIN){
+		return diff < (epsilon * FLT_MIN);
+	}
+	else{
+		return diff / fmin(fabs(a) + fabs(b), FLT_MAX) < epsilon;
+	}
+}
 typedef struct Point{float x; float y;} Point;
-typedef struct Line{struct Point p0; struct Point p1;} Line;
 
 void print_angle(float a){
 	printf("%f", a / M_TAU);
@@ -15,49 +27,54 @@ void print_point(Point p){
 	printf("%f, %f", p.x, p.y);
 }
 
-void print_line(Line l){
-	print_point(l.p0);
-	printf(" , ");
-	print_point(l.p1);
+float dist(Point A, Point B){
+	return sqrt(pow(B.x - A.x, 2) + pow(B.y - A.y, 2));
 }
 
-float line_dist(Line l){
-	return sqrt(pow(l.p1.x - l.p0.x, 2) + pow(l.p1.y - l.p0.y, 2));
-}
-
-Line line_create(float x0, float y0, float x1, float y1){
-	return (Line) { 
-			.p0 = (Point) { .x = x0, .y = y0 }, 
-			.p1 = (Point) {.x = x1, .y = y1}
-		};
-}
-Line line_create(Point p0, Point p1){
-	return (Line) { .p0 = p0, .p1 = p1 };
-}
 float get_opp_angle(float a, float b, float c){
 	return acos(
-		(pow(a, 2) + pow(c, 2) - pow(b, 2))
-		/ (2*a*c)
+			(pow(a, 2) + pow(c, 2) - pow(b, 2))
+			/ 
+			(2*a*c)
 		);
 }
 
-float get_offset_angle(float a, Line bc){
-	return acos(bc.p1.x/a);
+float get_offset_angle(float a, Point C_){
+	float angle = acos(C_.x/a);
+	printf("\noffset angle:");
+	print_angle(angle);
+	printf("\na*sin(angle): %f, C_.y: %f", a*sin(angle), C_.y);
+	if ( !nearly_equal(a*sin(angle), C_.y, FLT_EPSILON) ){
+		printf("\nnot equal");	
+		angle += M_TAU / 4;
+	}
+	return angle;
 }
 
-Point point_trilat(float b, float c, Line bc){
-	float a = line_dist(bc);
+Point point_trilat(float b, float c, Point B, Point C){
+	float a = dist(B, C);
 
-	float angle = get_opp_angle(a, b, c);
-	printf("angle abc:");
-	print_angle(angle);
-	angle += get_offset_angle(a, bc);
-	printf("\noffset of C:");
-	print_angle(get_offset(a, bc));
-	printf("\n");
+	printf("sides: %f, %f, %f\n", a, b, c);
+
+	Point C_ = { .x = C.x - B.x, .y = C.y - B.y };
+	printf("C_:");
+	print_point(C_);	
+	float ABC = get_opp_angle(a, b, c);
+	printf("\nangles:\nABC:");
+	print_angle(ABC);	
+
+	float C_B_C__ = get_offset_angle(a, C_);
+	printf("\nC_B_C__:");
+	print_angle(C_B_C__);
+
+	float A_B_C__ = ABC + C_B_C__;
+	printf("\nA_B_C__:");
+	print_angle(A_B_C__);
+	printf("\n");	
+	
 	return (Point) {
-		.x = bc.p0.x + c * cos(angle),
-		.y = bc.p0.y + c * sin(angle)
+		.x = B.x + c * cos(A_B_C__),
+		.y = B.y + c * sin(A_B_C__)
 		};
 }
 
@@ -66,13 +83,9 @@ int main( ){
 	Point B = { .x = 0, .y = 0};
 	Point C = { .x = 1, .y = 0};
 
-	Line ab = line_create(A, B);
-	Line bc = line_create(B, A);
-	Line ca = line_create(C, A);
-
-	float a = line_dist(bc);
-	float b = line_dist(ca);
-	float c = line_dist(ab);
+	float a = dist(B, C);
+	float b = dist(C, A);
+	float c = dist(A, B);
 
 	printf("Triangle with points:\nA:(");
 	print_point(A);
@@ -80,19 +93,19 @@ int main( ){
 	print_point(B);
 	printf("), C:(");
 	print_point(C);
-	printf(")\nsides:\na:%f, b:%f, c:%f\n"
+	printf(")\nsides:\na:%f, b:%f, c:%f\n",
 		a, b, c);
-
-	Point A_ = point_trilat(b, c, bc);
+	printf("-----------------\npoint A estimate:\n");
+	Point A_ = point_trilat(b, c, B, C);
 	printf("A:(");
 	print_point(A_);
-	printf(")\n");
-	Point B_ = point_trilat(c, a, ca);
+	printf(")\n----------------\npoint B estimate:\n");
+	Point B_ = point_trilat(c, a, C, A);
 	printf("B:(");
 	print_point(B_);
-	printf(")\n");
-	Point C_ = point_trilat(a, b, ab);
-	printf_("C:(");
+	printf(")\n-----------------\npoint C estimate:\n");
+	Point C_ = point_trilat(a, b, A, B);
+	printf("C:(");
 	print_point(C_);
 	printf(")\n");
 
